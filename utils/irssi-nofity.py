770dcd214850da8a1be45e1275af4cbe7ad7ea24
Josh <josh@josh-Precision-WorkStation-T5400.(none)>
2013-08-20 09:19:47 -0600

    add gitignore, update python script a bit

--- utils/irssi-nofity.py
+++ utils/irssi-nofity.py
@@ -10,9 +10,10 @@ Licensed under the CC BY-NC-SA 3.0
   http://creativecommons.org/licenses/by-nc-sa/3.0/
 """
 import zmq
-import requests
 import subprocess
 import arrow
+
+import pushover
 
 WHAT_R_WE_DOIN = "notify"
 WAIT_HOW_LONG  = 5
@@ -23,28 +24,15 @@ socket = context.socket(zmq.SUB)
 socket.connect (WHERE_IS_ZMQ)
 socket.setsockopt(zmq.SUBSCRIBE, 'irssi')
 
-pushover_api      = "https://api.pushover.net/1/messages.json"
-
-# TODO: Slight fuck up here. These should a) change, b) be env variables...
-pushover_user_key = "HuG1asKCjXHpNM45vSNzbxbGumjZcA"
-pushover_app_key  = "Z8qU23R3Dm5Bnsgu9pEcPacAE9ctRq"
-
-pushover_data     = {
-    "token":   pushover_app_key,
-    "user":    pushover_user_key,
-    "message": ""
-    }
 
 channel_bucket = {}
 
 
 def publish_pushover(data):
     data["date"] = data["date"].timestamp
-    data.update(pushover_data)
 
-    result = requests.post(pushover_api, data)
-    if result.json()["status"] != 1:
-        pass
+    try:
+      pushover.pushover(**data)
 
 
 def publish_notify(data):
@@ -56,6 +44,8 @@ def publish_notify(data):
 
 def listen():
     channel, message = socket.recv().split(' ', 1)[1].split(' ', 1)
+    print channel
+
     time = arrow.utcnow()
 
     if channel in channel_bucket:
@@ -77,6 +67,7 @@ def listen():
         if channel_bucket[channel] >= WAIT_HOW_LONG:
             channel_bucket[channel] = 0
             publish_pushover(local_data)
+
 
 if __name__ == "__main__":
     while True:
