fbee47914a41fac5ddd7ef26563002e5210ff513
JoshAshby <joshuaashby@joshashby.com>
2013-12-28 18:22:17 -0700

    start script for irssi-notify.py

---
+++ utils/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$STATE" ]; then
+  export VIRTUAL_ENV_DISABLE_PROMPT='1'
+  export STATE='true'
+
+  source ./virt/normal/bin/activate
+
+  export PUSHOVER_APP=wat
+  export PUSHOVER_USER=wat
+fi
+
+python irssi-notify.py
